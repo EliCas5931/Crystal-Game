a40634ca7a89a0c78a394aabc062f702bbef4a2a
Elicas5931 <elizabeth.perry.339@gmail.com>
2018-12-18 23:47:21 -0500

    Working on saving

---
+++ Assets/Javascript/game.js
@@ -0,0 +1,26 @@
+// Create the variables needed for this...
+var targetNumber;
+var winLoss;
+var crystals;
+var score;
+var userScore;
+
+// Target number the user is trying to get to...
+// Create a function to generate a random number for user to guess
+for (var i = 0; i < 4; i++) {
+
+var randomNumber = Math.floor(Math.random() * 12);
+console.log(randomNumber);
+
+var crystals = $("<div>");
+    crystals.attr("class", 'crystals');
+
+    $(".crystals").append(crystal);
+
+}
+
+// 4 crystals will generate random numbers 1-12
+
+// Random new number should generate every win/loss
+
+//
